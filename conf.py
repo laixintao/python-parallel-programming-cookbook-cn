41fa5d258c1eaf5b085eb313ac1ef19a517794db
laixintao <laixintao1995@163.com>
2017-09-17 23:54:08 +0800

    fix language

--- conf.py
+++ conf.py
@@ -64,7 +64,7 @@ release = u'1.0'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'en'
+language = 'zh_CN'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
