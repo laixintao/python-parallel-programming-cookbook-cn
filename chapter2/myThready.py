df734a0d15b6e88a5ab72e3b8a1bb6c5cc0f0bfc
laixintao <laixintao1995@163.com>
2017-09-26 14:10:50 +0800

    finish ch2.5

---
+++ chapter2/myThready.py
@@ -0,0 +1,38 @@
+import threading
+import time
+
+exitFlag = 0
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+
+    def run(self):
+        print("Starting " + self.name)
+        print_time(self.name, self.counter, 5)
+        print("Exiting " + self.name)
+
+def print_time(threadName, delay, counter):
+    while counter:
+        if exitFlag:
+            thread.exit()
+        time.sleep(delay)
+        print("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
+
+# Create new threads
+thread1 = myThread(1, "Thread-1", 1)
+thread2 = myThread(2, "Thread-2", 2)
+
+# Start new Threads
+thread1.start()
+thread2.start()
+
+# 以下两行为译者添加，如果要获得和图片相同的结果，
+# 下面两行是必须的。疑似原作者的疏漏
+thread1.join()
+thread2.join()
+print("Exiting Main Thread")
